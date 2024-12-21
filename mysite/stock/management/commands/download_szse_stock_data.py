f0cad04140e61fcffbc1fd92fe69d2e351e31fe4
lian <imwhatiam123@gmail.com>
2024-12-21 14:04:57 +0800

    update download_szse_stock_data.py

--- mysite/stock/management/commands/download_szse_stock_data.py
+++ mysite/stock/management/commands/download_szse_stock_data.py
@@ -1,7 +1,8 @@
 import os
 import random
 import requests
-import datetime
+from datetime import datetime, timedelta
+from chinese_calendar import is_holiday
 
 from django.core.management.base import BaseCommand
 
@@ -15,6 +16,27 @@ if not os.path.exists(STOCK_EXCEL_PARENT_FOLDER):
     os.makedirs(STOCK_EXCEL_PARENT_FOLDER)
 
 
+def is_future_date(date_str):
+    return datetime.strptime(date_str, "%Y-%m-%d").date() > datetime.today().date()
+
+
+def get_date_list(start_date_str='',
+                  end_date_str=datetime.today().strftime('%Y-%m-%d')):
+
+    if not start_date_str:
+        return [end_date_str]
+
+    start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
+    end_date = datetime.strptime(end_date_str, "%Y-%m-%d")
+
+    date_list = []
+    while start_date <= end_date:
+        date_list.append(start_date.strftime("%Y-%m-%d"))
+        start_date += timedelta(days=1)
+
+    return date_list
+
+
 class Command(BaseCommand):
 
     help = "Download stock Excel file"
@@ -22,40 +44,75 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
 
         parser.add_argument(
-            "--date",
+            "--start-date",
             type=str,
-            nargs="?",  # Makes 'date' argument optional
-            default=datetime.date.today().strftime('%Y-%m-%d'),
+            nargs="?",  # Makes 'start-date' argument optional
+            default='',
             help="Date in YYYY-MM-DD format"
         )
         parser.add_argument(
-            "--stock_excel_parent_folder",
+            "--end-date",
+            type=str,
+            nargs="?",  # Makes 'end-date' argument optional
+            default=datetime.today().strftime('%Y-%m-%d'),
+            help="Date in YYYY-MM-DD format"
+        )
+
+        parser.add_argument(
+            "--stock-excel-parent-folder",
             type=str,
             nargs="?",  # Makes 'date' argument optional
             default=STOCK_EXCEL_PARENT_FOLDER,  # Default save path
-            help="Path to save the downloaded file (default: /tmp/)"
+            help="Path to save the downloaded file"
         )
 
     def handle(self, *args, **kwargs):
 
-        random_value = random.random()
-        random_value = f"{random_value:.15f}"
+        start_date_str = kwargs["start_date"]
+        end_date_str = kwargs["end_date"]
 
-        date_str = kwargs["date"]
-        stock_excel_parent_folder = normalize_folder_path(kwargs["stock_excel_parent_folder"])
+        if start_date_str and is_future_date(start_date_str):
+            error_msg = f"Start date {start_date_str} is a future date! "
+            self.stderr.write(self.style.ERROR(error_msg))
+            return
 
-        url = (
-            "https://www.szse.cn/api/report/ShowReport"
-            "?SHOWTYPE=xlsx&CATALOGID=1815_stock_snapshot&TABKEY=tab1&"
-            f"txtBeginDate={date_str}&txtEndDate={date_str}&"
-            "archiveDate=2022-12-01&random={random_value}"
-        )
-        response = requests.get(url)
+        if end_date_str and is_future_date(end_date_str):
+            error_msg = f"End date {end_date_str} is a future date! "
+            self.stderr.write(self.style.ERROR(error_msg))
+            return
 
-        if response.status_code == 200:
+        date_list = get_date_list(start_date_str, end_date_str)
+
+        for date_str in date_list:
+
+            date_obj = datetime.strptime(date_str, "%Y-%m-%d").date()
+            if is_holiday(date_obj):
+                error_msg = f"{date_str} is a holiday! "
+                self.stderr.write(self.style.ERROR(error_msg))
+                continue
+
+            random_value = random.random()
+            random_value = f"{random_value:.15f}"
+
+            stock_excel_parent_folder = normalize_folder_path(kwargs["stock_excel_parent_folder"])
             stock_excel_file = f"{stock_excel_parent_folder}stock_data_{date_str}.xlsx"
-            with open(stock_excel_file, 'wb') as f:
-                f.write(response.content)
-            print(f"Excel file downloaded and saved to {stock_excel_file}")
-        else:
-            print(f"Failed to download file. Status code: {response.status_code}")
+
+            if os.path.exists(stock_excel_file):
+                error_msg = f"File already exists: {stock_excel_file}"
+                self.stderr.write(self.style.ERROR(error_msg))
+                continue
+
+            url = (
+                "https://www.szse.cn/api/report/ShowReport"
+                "?SHOWTYPE=xlsx&CATALOGID=1815_stock_snapshot&TABKEY=tab1&"
+                f"txtBeginDate={date_str}&txtEndDate={date_str}&"
+                "archiveDate=2022-12-01&random={random_value}"
+            )
+            response = requests.get(url)
+
+            if response.status_code == 200:
+                with open(stock_excel_file, 'wb') as f:
+                    f.write(response.content)
+                print(f"Excel file downloaded and saved to {stock_excel_file}")
+            else:
+                print(f"Failed to download {date_str} file. Status code: {response.status_code}")
--- mysite/stock/views.py
+++ mysite/stock/views.py
@@ -1,7 +1,72 @@
-from django.shortcuts import render
+import pandas as pd
+from decimal import Decimal
+from django.http import HttpResponse
 from .models import StockBasicInfo
 
 
 def fupan(request):
 
-    return render(request, 'stock/fupan.html')
+    # get all code
+    # [{'code': '000001'}, {'code': '000002'}, ...]
+    distinct_codes = StockBasicInfo.objects.values('code').distinct()
+    codes_list = [code_dict['code'] for code_dict in distinct_codes]
+
+    codes_list = ['603990']
+    for code in codes_list:
+
+        # get latest 10 days data for each stock
+        basic_infos = StockBasicInfo.objects.filter(code=code).order_by('-date')[:20]
+
+        data_list = []
+        for basic_info in basic_infos:
+            data_list.append({
+                'date': basic_info.date,
+                'code': basic_info.code,
+                'name': basic_info.name,
+                'open_price': basic_info.open_price,
+                'close_price': basic_info.close_price,
+                'high_price': basic_info.high_price,
+                'low_price': basic_info.low_price,
+                'money': basic_info.money
+            })
+
+        df = pd.DataFrame(data_list)
+        print(df)
+        df = df.sort_values(by='date', ascending=False)
+
+        # 计算每日涨幅（相较于前一天的涨幅）
+        df['change_percentage'] = df.groupby('code')['close_price'].pct_change() * 100
+        print(df)
+
+        def analyze_stock_data(df):
+            first_high_change = df[df['change_percentage'] > 8]
+            if first_high_change.empty:
+                return None
+
+            # 获取第一次涨幅大于 8% 的日期及相关收盘价
+            first_high_date = first_high_change.iloc[0]['date']
+            high_close_price = df[df['date'] == first_high_date].iloc[0]['close_price']
+
+            # 计算 95% 的阈值
+            # threshold_price = high_close_price * Decimal('0.98')
+            threshold_price = high_close_price
+
+            # 2. 过滤条件
+            filtered_data = df[df['date'] > first_high_date]
+            filtered_data = filtered_data[
+                (filtered_data['close_price'] >= threshold_price) &
+                (filtered_data['change_percentage'].abs() <= 3)
+            ]
+
+            if filtered_data.empty:
+                return None
+
+            return filtered_data
+
+        # 示例调用
+        result = analyze_stock_data(df)
+
+        if result is not None:
+            print(result)
+
+    return HttpResponse()
