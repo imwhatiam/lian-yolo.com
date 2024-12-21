e16fdae9208f967ecc3d19eb444a32d0aa013fef
lian <li_an_1986@163.com>
2024-12-21 16:35:40 +0800

    update

--- mysite/bash_script/scrape_douban_jiaoyou.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-echo "$(date '+%Y-%m-%d %H:%M:%S') - Running scrape_douban_jiaoyou.sh"
-source /root/django-5.0/bin/activate && cd /root/lian-yolo.com/mysite && python manage.py scrape_jiaoyou
