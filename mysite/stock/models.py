37b0ea9c0cf8955f2430d3d865fa54565a4a9657
lian <li_an_1986@163.com>
2025-01-17 18:34:02 +0800

    update

--- mysite/stock/models.py
+++ mysite/stock/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Industris(models.Model):
 
     code = models.CharField(max_length=10, db_index=True, unique=True)
-    name = models.CharField(max_length=10, db_index=True)
+    name = models.CharField(max_length=100, db_index=True)
     level = models.CharField(max_length=10, db_index=True)
 
     def __str__(self):
@@ -19,7 +19,7 @@ class Industris(models.Model):
 class Stocks(models.Model):
 
     code = models.CharField(max_length=10, db_index=True, unique=True)
-    name = models.CharField(max_length=10, db_index=True, unique=True)
+    name = models.CharField(max_length=100, db_index=True, unique=True)
     sw_l2 = models.CharField(max_length=10, db_index=True)
     sw_l3 = models.CharField(max_length=10, db_index=True)
 
@@ -41,7 +41,7 @@ class StockBasicInfo(models.Model):
 
     date = models.DateField(db_index=True)
     code = models.CharField(max_length=10, db_index=True)
-    name = models.CharField(max_length=10, db_index=True)
+    name = models.CharField(max_length=100, db_index=True)
 
     open_price = models.DecimalField(max_digits=10, decimal_places=2)
     close_price = models.DecimalField(max_digits=10, decimal_places=2)
