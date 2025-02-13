d5ef0e889aec57dd667207c053ba5cbae553c6d6
lian <li_an_1986@163.com>
2025-02-13 16:46:53 +0800

    update

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -170,7 +170,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://lian-yolo-frontend.vercel.app"
-    "https://stock.lian-yolo.com"
 ]
 
 # for dev env
