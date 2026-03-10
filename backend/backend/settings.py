62bdb1aa460331faf8f8c923a19faeba9a79982f
lian <li_an_1986@163.com>
2026-03-10 14:24:40 +0800

    update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9m%v^676#51-!9icf8d5u-)ox7!t$@e)m^*4n%nlf4af*$1+&&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.lian-yolo.com', 'lian-yolo.com']
+CSRF_TRUSTED_ORIGINS = ['https://www.lian-yolo.com', 'https://lian-yolo.com']
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")   # collected by collectstatic
 
 
 # Application definition
--- backend/weixin_miniprogram/admin.py
+++ backend/weixin_miniprogram/admin.py
@@ -1,126 +1,23 @@
-from datetime import datetime
+import time
 from django.contrib import admin
-from django.utils.html import format_html
-
-from .models import Activities, WeixinUserInfo
+from .models import Activities, WeixinUserInfo, current_timestamp
 
 
 @admin.register(Activities)
-class ActivityAdmin(admin.ModelAdmin):
-    list_display = [
-        'title',
-        'activity_type',
-        'creator_avatar',
-        'formatted_items',
-        'formatted_white_list',
-        'formatted_last_modified',
-    ]
+class ActivitiesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'activity_title', 'creator_weixin_name', 'activity_type', 'last_modified')
+    list_filter = ('activity_type', 'last_modified')
+    search_fields = ('activity_title', 'creator_weixin_name', 'creator_weixin_id')
+    readonly_fields = ('last_modified',)  # 设置为只读，通过 save_model 自动更新
 
-    search_fields = ['activity_title', 'creator_weixin_name', 'creator_weixin_id']
-    list_per_page = 20
-    list_filter = ('activity_type',)
-    ordering = ('-last_modified',)
-
-    def title(self, obj):
-        return obj.activity_title
-
-    title.short_description = "Title"
-
-    def activity_type(self, obj):
-        return obj.activity_type
-
-    activity_type.short_description = "Type"
-
-    def formatted_last_modified(self, obj):
-        if not obj.last_modified:
-            return "-"
-        return datetime.fromtimestamp(obj.last_modified).strftime("%Y-%m-%d %H:%M:%S")
-
-    formatted_last_modified.short_description = "Last Modified"
-
-    def creator_avatar(self, obj):
-        """Show the creator's Weixin avatar."""
-        try:
-            user = WeixinUserInfo.objects.get(weixin_id=obj.creator_weixin_id)
-            if user.avatar:
-                return format_html(
-                    '<img src="{}" width="50" height="50" style="border-radius: 50%;" title="{}" />',
-                    user.avatar.url,
-                    user.nickname or user.weixin_id,
-                )
-            else:
-                return user.nickname or user.weixin_id
-        except WeixinUserInfo.DoesNotExist:
-            return obj.creator_weixin_name or obj.creator_weixin_id
-
-    creator_avatar.short_description = "Creator"
-
-    def formatted_items(self, obj):
-        """Pretty print activity_items JSON"""
-        try:
-            items = obj.activity_items
-            if not items:
-                return "-"
-            lines = []
-            for key, value in items.items():
-                name = value.get("name", "")
-                status = value.get("status", "")
-                operator = value.get("operator", "")
-                lines.append(f"<b>{key}</b>: {name} (status: {status}, operator: {operator})")
-            return format_html("<br>".join(lines))
-        except Exception:
-            return format_html("<span style='color:red;'>Invalid JSON</span>")
-
-    formatted_items.short_description = "Items"
-    formatted_items.allow_tags = True
-
-    def formatted_white_list(self, obj):
-        """Pretty print white_list JSON"""
-        try:
-            white_list = obj.white_list
-            if not white_list:
-                return "-"
-            lines = []
-            for entry in white_list:
-                weixin_id = entry.get("weixin_id", "")
-                permission = entry.get("permission", "")
-                avatar_url = entry.get("avatar_url", "")
-                if avatar_url:
-                    lines.append(
-                        f'<img src="{avatar_url}" width="25" height="25" style="border-radius:50%; margin-right:5px;">'
-                        f'{weixin_id} ({permission})'
-                    )
-                else:
-                    lines.append(f"{weixin_id} ({permission})")
-            return format_html("<br>".join(lines))
-        except Exception:
-            return format_html("<span style='color:red;'>Invalid JSON</span>")
-
-    formatted_white_list.short_description = "White List"
-    formatted_white_list.allow_tags = True
+    def save_model(self, request, obj, form, change):
+        """在更新操作时自动更新 last_modified 字段"""
+        if change:
+            obj.last_modified = current_timestamp()
+        super().save_model(request, obj, form, change)
 
 
 @admin.register(WeixinUserInfo)
 class WeixinUserInfoAdmin(admin.ModelAdmin):
-    list_display = ['weixin_id', 'nickname', 'avatar_image']
-    search_fields = ['weixin_id', 'nickname']
-    readonly_fields = ['avatar_preview']
-
-    def avatar_image(self, obj):
-        if obj.avatar:
-            return format_html(
-                '<img src="{}" width="50" height="50" style="border-radius: 50%;" />',
-                obj.avatar.url
-            )
-        return "无头像"
-    avatar_image.short_description = '头像'
-
-    def avatar_preview(self, obj):
-        if obj.avatar:
-            return format_html(
-                '<img src="{}" width="150" height="150" style="border-radius: 50%;" />',
-                obj.avatar.url
-            )
-        return "无头像"
-
-    avatar_preview.short_description = '头像预览'
+    list_display = ('weixin_id', 'nickname', 'avatar')
+    search_fields = ('weixin_id', 'nickname')
